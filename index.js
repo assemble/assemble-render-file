55003e949ee9e300e6a4a8c13f9cf88462def3a1
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-30 22:06:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+/*!
+ * assemble-render-file <https://github.com/jonschlinkert/assemble-render-file>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+var utils = require('./utils');
+
+/**
+ * Render a vinyl file.
+ *
+ * ```js
+ * app.src('*.hbs')
+ *   .pipe(app.renderFile());
+ * ```
+ *
+ * @name .renderFile
+ * @param  {Object} `locals` Optionally locals to pass to the template engine for rendering.
+ * @return {Object}
+ * @api public
+ */
+
+module.exports = function renderFile(app) {
+  return function (locals) {
+    var opts = {};
+    if (locals && !locals.isCollection) {
+      opts = utils.merge({}, app.options, locals);
+    }
+    var collection = app.collection(opts);
+    var File = opts.File || utils.File;
+
+    return utils.through.obj(function (file, enc, next) {
+      if (file.isNull()) {
+        return cb(null, file);
+      }
+
+      if (!file.isView) {
+        file = collection.view(file);
+      }
+
+      // run `onLoad` middleware
+      app.handleView('onLoad', file);
+
+      // create the context to pass to templates
+      var ctx = utils.merge({}, app.cache.data, locals, file.data);
+
+      // render the file
+      app.render(file, ctx, function (err, res) {
+        if (err) return next(err);
+        next(null, new File(res));
+      });
+    });
+  };
+};
