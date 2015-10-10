0b6762a646a8584458e3d61ee2134c5d1c1ef665
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-10 11:41:30 -0400

    make lazy-cached requires browserify-able

--- utils.js
+++ utils.js
@@ -4,8 +4,16 @@
  * Module dependencies
  */
 
-var lazy = require('lazy-cache')(require);
-lazy('mixin-deep', 'merge');
-lazy('through2', 'through');
-lazy('vinyl', 'File');
-module.exports = lazy;
+var utils = require('lazy-cache')(require);
+var fn = require;
+
+require = utils;
+require('mixin-deep', 'merge');
+require('through2', 'through');
+require = fn;
+
+/**
+ * Expose `utils`
+ */
+
+module.exports = utils;
