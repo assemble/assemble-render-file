2042090ff878e2c916e9cd167532d5ad8af28d27
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-17 08:59:24 -0500

    use `eslint`

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
+'use strict';
+
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
-var stylish = require('jshint-stylish');
 var istanbul = require('gulp-istanbul');
-var jshint = require('gulp-jshint');
+var eslint = require('gulp-eslint');
+
+var lint = ['index.js', 'utils.js', 'test.js'];
 
 gulp.task('coverage', function () {
   return gulp.src('index.js')
@@ -11,18 +14,14 @@ gulp.task('coverage', function () {
 });
 
 gulp.task('mocha', ['coverage'], function () {
-  return gulp.src('test/*.js')
+  return gulp.src('test.js')
     .pipe(mocha({reporter: 'spec'}))
-    .pipe(istanbul.writeReports({
-      reporters: [ 'text' ],
-      reportOpts: {dir: 'coverage', file: 'summary.txt'}
-    }))
+    .pipe(istanbul.writeReports())
 });
 
-gulp.task('jshint', function () {
-  return gulp.src('index.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
+gulp.task('eslint', function () {
+  return gulp.src(lint)
+    .pipe(eslint())
 });
 
-gulp.task('default', ['mocha', 'jshint']);
+gulp.task('default', ['mocha', 'eslint']);
