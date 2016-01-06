a3809217dfd2f424f6b3f29d7dd350ac92e74dfb
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-06 08:45:37 -0500

    lint

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ var path = require('path');
 var app;
 
 describe('app.renderFile()', function() {
-  beforeEach(function () {
+  beforeEach(function() {
     app = new Assemble()
       .use(renderFile())
 
@@ -22,11 +22,11 @@ describe('app.renderFile()', function() {
     app.file('b', {content: 'this is <%= title() %>'});
     app.file('c', {content: 'this is <%= title() %>'});
 
-    app.option('renameKey', function (key) {
+    app.option('renameKey', function(key) {
       return path.basename(key, path.extname(key));
     });
 
-    app.helper('title', function () {
+    app.helper('title', function() {
       if (this.context.title) {
         return this.context.title;
       }
@@ -38,86 +38,105 @@ describe('app.renderFile()', function() {
     });
   });
 
-  it('should render views from src', function (done) {
+  it('should render views from src', function(cb) {
     var stream = app.src(path.join(__dirname, 'fixtures/*.hbs'));
     var files = [];
 
     stream.pipe(app.renderFile())
-      .on('error', done)
-      .on('data', function (file) {
+      .on('error', cb)
+      .on('data', function(file) {
         files.push(file);
       })
-      .on('end', function () {
+      .on('end', function() {
         assert.equal(files[0].basename, 'a.hbs');
         assert.equal(files[1].basename, 'b.hbs');
         assert.equal(files[2].basename, 'c.hbs');
-        done();
+        cb();
       });
   });
 
-  it('should render views with the engine that matches the file extension', function (done) {
+  it('should render views with the engine that matches the file extension', function(cb) {
     var stream = app.src(path.join(__dirname, 'fixtures/*.hbs'));
     var files = [];
 
     stream.pipe(app.renderFile())
-      .on('error', done)
-      .on('data', function (file) {
+      .on('error', cb)
+      .on('data', function(file) {
         files.push(file);
       })
-      .on('end', function () {
+      .on('end', function() {
         assert(/<h1>a<\/h1>/.test(files[0].content));
         assert(/<h1>b<\/h1>/.test(files[1].content));
         assert(/<h1>c<\/h1>/.test(files[2].content));
-        done();
+        cb();
       });
   });
 
-  it('should render views from src with the engine passed on the opts', function (done) {
+  it('should render views from src with the engine passed on the opts', function(cb) {
     var stream = app.src(path.join(__dirname, 'fixtures/*.hbs'));
     var files = [];
 
     stream.pipe(app.renderFile({engine: '*'}))
-      .on('error', done)
-      .on('data', function (file) {
+      .on('error', cb)
+      .on('data', function(file) {
         files.push(file);
       })
-      .on('end', function () {
+      .on('end', function() {
         assert(/<h2>a<\/h2>/.test(files[0].content));
         assert(/<h2>b<\/h2>/.test(files[1].content));
         assert(/<h2>c<\/h2>/.test(files[2].content));
-        done();
+        cb();
       });
   });
 
-  it('should use the context passed on the opts', function (done) {
+  it('should use the context passed on the opts', function(cb) {
     var stream = app.src(path.join(__dirname, 'fixtures/*.hbs'));
     var files = [];
 
     stream.pipe(app.renderFile({a: {title: 'foo'}}))
-      .on('error', done)
-      .on('data', function (file) {
+      .on('error', cb)
+      .on('data', function(file) {
         files.push(file);
       })
-      .on('end', function () {
+      .on('end', function() {
         assert(/<h1>foo<\/h1>/.test(files[0].content));
         assert(/<h1>b<\/h1>/.test(files[1].content));
         assert(/<h1>c<\/h1>/.test(files[2].content));
-        done();
+        cb();
       });
   });
 
-  it('should render the files in a collection', function (cb) {
+  it('should support noop engines', function(cb) {
+    var stream = app.src(path.join(__dirname, '.*'));
+    var files = [];
+
+    // app.engine('noop', function(view, opts, next) {
+    //   next(null, view);
+    // });
+
+    stream.pipe(app.renderFile())
+      .on('error', cb)
+      .on('data', function(file) {
+        files.push(file);
+      })
+      .on('end', function() {
+        console.log(files)
+        cb();
+      });
+  });
+
+  it('should render the files in a collection', function(cb) {
     var files = [];
     app.toStream('files')
       .pipe(app.renderFile())
       .on('error', cb)
-      .on('data', function (file) {
+      .on('data', function(file) {
         assert(file);
         assert(file.path);
         assert(file.contents);
         files.push(file);
       })
-      .on('end', function () {
+      .on('end', function() {
         assert(/this is a/.test(files[0].content));
         assert(/this is b/.test(files[1].content));
         assert(/this is c/.test(files[2].content));
@@ -126,25 +145,25 @@ describe('app.renderFile()', function() {
       });
   });
 
-  it('should handle engine errors', function (cb) {
+  it('should handle engine errors', function(cb) {
     var files = [];
     app.create('notdefined', {engine: '*'});
     app.notdefined('foo', {content: '<%= bar %>'})
     app.toStream('notdefined')
       .pipe(app.renderFile())
-      .on('error', function (err) {
+      .on('error', function(err) {
         assert.equal(typeof err, 'object');
         assert.equal(err.message, 'bar is not defined');
         cb();
       })
-      .on('end', function () {
+      .on('end', function() {
         cb(new Error('expected renderFile to handle the error.'));
       });
   });
 });
 
 describe('app.renderFile()', function() {
-  beforeEach(function () {
+  beforeEach(function() {
     app = new Assemble()
       .use(renderFile())
 
@@ -157,37 +176,37 @@ describe('app.renderFile()', function() {
     app.data({title: 'foo'});
   });
 
-  it('should render views with the engine specified on arguments', function (done) {
+  it('should render views with the engine specified on arguments', function(cb) {
     var stream = app.src(path.join(__dirname, 'fixtures/engines/*.hbs'));
     var files = [];
 
     stream.pipe(app.renderFile('foo'))
-      .on('error', done)
-      .on('data', function (file) {
+      .on('error', cb)
+      .on('data', function(file) {
         // console.log(file.content);
         files.push(file);
       })
-      .on('end', function () {
+      .on('end', function() {
         assert(/<h2>foo<\/h2>/.test(files[0].content));
         assert(/<h2>foo<\/h2>/.test(files[1].content));
         assert(/<h2>foo<\/h2>/.test(files[2].content));
-        done();
+        cb();
       });
   });
 
-  it('should render views with multiple calls to renderFile', function (done) {
+  it('should render views with multiple calls to renderFile', function(cb) {
     var stream = app.src(path.join(__dirname, 'fixtures/engines/*.hbs'));
     var files = [];
 
     stream
       .pipe(app.renderFile('foo'))
       .pipe(app.renderFile('hbs'))
-      .on('error', done)
-      .on('data', function (file) {
+      .on('error', cb)
+      .on('data', function(file) {
         // console.log(file.content)
         files.push(file);
       })
-      .on('end', function () {
+      .on('end', function() {
         assert(/<h1>foo<\/h1>/.test(files[0].content));
         assert(/<h1>foo<\/h1>/.test(files[1].content));
         assert(/<h1>foo<\/h1>/.test(files[2].content));
@@ -195,11 +214,11 @@ describe('app.renderFile()', function() {
         assert(/<h2>foo<\/h2>/.test(files[0].content));
         assert(/<h2>foo<\/h2>/.test(files[1].content));
         assert(/<h2>foo<\/h2>/.test(files[2].content));
-        done();
+        cb();
       });
   });
 
-  it('should render views with multiple calls to renderFile and locals', function (done) {
+  it('should render views with multiple calls to renderFile and locals', function(cb) {
 
     var stream = app.src(path.join(__dirname, 'fixtures/engines/a.hbs'));
     var files = [];
@@ -207,16 +226,16 @@ describe('app.renderFile()', function() {
 
     stream
       .pipe(app.renderFile('foo', {title: 'foo'}))
-      .on('error', done)
+      .on('error', cb)
       .pipe(app.renderFile('hbs', {title: 'bar'}))
-      .on('error', done)
-      .on('data', function (file) {
+      .on('error', cb)
+      .on('data', function(file) {
         files.push(file);
       })
-      .on('end', function () {
+      .on('end', function() {
         assert(/<h1>bar<\/h1>/.test(files[0].content));
         assert(/<h2>foo<\/h2>/.test(files[0].content));
-        done();
+        cb();
       });
   });
 });
